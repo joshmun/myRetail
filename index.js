0bcbfe7f9a0b64f8610136e4d5030634f4af08e3
joshmun <joshuamun@gmail.com>
2018-03-03 12:45:56 -0800

    adding hidden keys

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+mongoose.connect();
+
 const app = express();
 
 app.get('/', (req, res) => {
