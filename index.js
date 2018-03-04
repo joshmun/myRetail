c4e79f2c1031d3fd4a9e81e2371e973e5b4fb4ff
joshmun <joshuamun@gmail.com>
2018-03-03 16:20:00 -0800

    add dev and test environment scripts to implement dev and test db

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const env = process.env.NODE_ENV;
 
-mongoose.connect(keys.mongoURI);
+console.log(keys.mongoURI);
+console.log(env);
+console.log(keys.mongoURI[env]);
+
+mongoose.connect(keys.mongoURI[env]);
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send("hello world");
 });
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
