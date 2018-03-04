2f3fa5c435c4e2aa9338c1d537beb6e75bdfe2e5
joshmun <joshuamun@gmail.com>
2018-03-03 16:57:04 -0800

    clean up code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const common = require('./common');
+const common = require('./config/common');
 const config = common.config();
 
 mongoose.connect(config.mongoURI);
