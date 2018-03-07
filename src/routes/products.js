a22106eef689a7ac6f354983e50b239330a895e6
joshmun <joshuamun@gmail.com>
2018-03-07 14:32:24 -0800

    add axios package

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,24 +1,24 @@
 const router = require('express').Router();
 const products = require('../services/products');
 
-// GET all
-router.get('/', (req,res) => {
-  products.getProduct(res);
-});
+// // GET all
+// router.get('/', (req,res) => {
+//   products.getProduct(res);
+// });
 
 //  GET /products/:id
 router.get('/:id', (req,res) => {
   products.getProductById(res, req.params.id);
 });
 
-// POST creates new product
-router.post('/', (req,res) => {
-  products.postProduct(res);
-});
+// // POST creates new product
+// router.post('/', (req,res) => {
+//   products.postProduct(res);
+// });
 
 // DELETE /products/:id
-router.delete('/:id', (req,res) => {
-  products.deleteProduct(res, req.params.id);
-});
+// router.delete('/:id', (req,res) => {
+//   products.deleteProduct(res, req.params.id);
+// });
 
 module.exports = router;
